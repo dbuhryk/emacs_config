6558c3ec689d3216bf194f5d067aaf5ac7c9facc
Dzmitry Buhryk <dmitry.bugrik@gmail.com>
2015-02-13 12:27:06 +0100

    add cygwin

--- init.d/01-emacs_environment.el
+++ init.d/01-emacs_environment.el
@@ -32,6 +32,14 @@
       (tool-bar-mode -1)))
 
 ;;****************************************************************
+;; ORG mode
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+
+;;****************************************************************
 ;; windmove
 (global-set-key [M-right] `windmove-right)
 (global-set-key [M-left] `windmove-left)
